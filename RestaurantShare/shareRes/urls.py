bce034c3303fb6ddac09b969068342c2b37373ab
laits1 <thsehdrl1@gmail.com>
2021-08-30 17:54:12 +0900

    2021년 08월 30일 월 오후  5:54:12

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # http://localhost:8000 요청 처리
     path('restaurantDetail/', views.restaurantDetail, name='resDetailPage'), #http://localhost:8000/restaurantDetail/ 요청 처리
     path('restaurantCreate/', views.restaurantCreate, name='resCreatePage'), #http://localhost:8000/restaurantCreate/ 요청 처리
-    path('restaurantCreate/', views.Create_restaurant, name='resCreate'),
     path('categoryCreate/', views.categoryCreate, name='cateCreatePage'), # http://localhost:8000/categoryCreate/ 요청 처리
     path('categoryCreate/create', views.Create_category, name = 'cateCreate'),
-    path('categoryCreate/delete', views.Delete_category, name = 'cateDelete')
-
-]
+    ]
